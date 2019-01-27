39b533dd0312957e922bdfe778b5bfadd20194d4
QaddafiSezili <qaddafisezili@gmail.com>
2019-01-27 16:11:01 +0800

    Add score dummy initial values

--- js/setting.js
+++ js/setting.js
@@ -6,6 +6,7 @@ var w = 350;
 var h = 350;
 var scoreArray = []; // array to store score
 var scoreDummyArray = []; // array to store score
+var scoreDummy = 0;
 var score_food_1 = 1; // food score 1
 var score_food_2 = 2; // food score 2
 var snake;
